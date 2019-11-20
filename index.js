f76b2cc1bce13a37d6476d9fbaaf4010fc840bb1
bitfriend <bitfriend@hotmail.com>
2019-11-20 22:38:53 +0300

    Fixed problem

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-// Use the code outline below for your answer
-
 var readline = require('readline').createInterface({
   input: process.stdin,
   output: process.stdout
@@ -10,20 +8,34 @@ Employee = function(staffMember) {
    this.familyname = staffMember.familyname
    this.age = staffMember.age
 
-   this.setName = function(newName) { this.firstname = newName }
-   this.setFullName = function() { return this.name + ' ' +  this.familyname }
+   this.getFullName = function() {
+     var fullName = [];
+     if (!!this.firstname) {
+       fullName.push(this.firstname);
+     }
+     if (!!this.familyname) {
+       fullName.push(this.familyname);
+     }
+     return fullName.join(' ');
+   }
 };
 
 
 readline.on('line', function(line) {
-  counter+=1;
-  staffRoster[counter] = function() {
-    var staff = new Employee({ firstname : line.trim() });
-    console.log("Staff #" + counter + ': ' + staff.firstname + ' ' + staff.familyname + ',' + staff.age);
+  counter++;
+  var rx = /^([a-zA-Z]*)[, ]+([a-zA-Z]*),(\d+)$/g;
+  staffRoster[counter] = function(index) {
+    var data = rx.exec(line.trim());
+    if (data) {
+      var staff = new Employee({ firstname : data[1], familyname: data[2], age: data[3] });
+      console.log('#' + index + ',' + staff.getFullName() + ',' + staff.age);
+    } else {
+      console.log('#' + index + ',---Error---');
+    }
   };
 }).on('close', function() {
-  staffRoster.forEach(function(element){
-    element();
+  staffRoster.forEach(function(element, index){
+    element(index);
   });
   console.log('Have a great day!');
   process.exit(0);
